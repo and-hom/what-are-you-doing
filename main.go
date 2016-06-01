95ef78ff6e2b1442589bfc45e76b7dca00b54903
and-hom <and-hom@yandex-team.ru>
2016-06-01 20:31:38 +0300

    tray experiments

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 	"github.com/gotk3/gotk3/glib"
 	"github.com/jasonlvhit/gocron"
 	"fmt"
+	"os"
+	"image"
+	"github.com/axet/desktop/go"
 )
 
 const (
@@ -114,9 +117,60 @@ func mkgrid(orientation gtk.Orientation) *gtk.Grid {
 	return grid
 }
 
+type SysTest struct {
+	s *desktop.DesktopSysTray
+}
+
+func (m *SysTest) Click(mn *desktop.Menu) {
+	fmt.Println("m", mn.Name)
+}
+
+func (m *SysTest) ClickBox(mn *desktop.Menu) {
+	fmt.Println(mn.Name)
+	mn.State = !mn.State
+	m.s.Update()
+}
+
+
 func main() {
+
+
 	log.SetFormatter(&log.TextFormatter{FullTimestamp:true, TimestampFormat:"2006-01-02 15:04:05 -0700"})
 	log.Info("Starting...")
+
+
+	m := SysTest{desktop.DesktopSysTrayNew()}
+
+	file, err := os.Open("icon.png")
+	if err != nil {
+		panic(err)
+	}
+	icon, _, err := image.Decode(file)
+	if err != nil {
+		panic(err)
+	}
+
+	menu := []desktop.Menu{
+		desktop.Menu{Icon: icon, Type: desktop.MenuItem, Enabled: true, Name: "test1", Action: m.Click},
+		desktop.Menu{Type: desktop.MenuSeparator},
+		desktop.Menu{Icon: icon, Type: desktop.MenuItem, Enabled: true, Name: "test2", Menu: []desktop.Menu{
+			desktop.Menu{Type: desktop.MenuItem, Enabled: true, Name: "test21", Action: m.Click},
+			desktop.Menu{Type: desktop.MenuItem, Enabled: true, Name: "test22", Action: m.Click},
+		}},
+		desktop.Menu{Type: desktop.MenuItem, Enabled: false, Name: "test3", Action: m.Click},
+		desktop.Menu{Type: desktop.MenuCheckBox, Enabled: true, Name: "test4", State: true, Action: m.ClickBox},
+		desktop.Menu{Type: desktop.MenuSeparator},
+		desktop.Menu{Icon: icon, Type: desktop.MenuItem, Enabled: true, Name: "test5", Action: m.Click},
+	}
+
+	m.s.SetIcon(icon)
+	m.s.SetTitle("go menu hoho!")
+	m.s.SetMenu(menu)
+	m.s.Show()
+
+	desktop.Main()
+
+
 
 	// Initialize GTK without parsing any command line arguments.
 	gtk.Init(nil)
