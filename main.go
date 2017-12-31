f0c19fd9a0bceb68020261a8e8d294d8c3e43c72
Андрей Хомяков <and.hom@gmail.com>
2018-01-01 01:16:50 +0300

    migrate to QT

--- main.go
+++ main.go
@@ -2,47 +2,27 @@ package main
 
 import (
 	log "github.com/Sirupsen/logrus"
-	"github.com/gotk3/gotk3/gtk"
-	"github.com/gotk3/gotk3/glib"
-	"github.com/jasonlvhit/gocron"
+
 	"fmt"
 	"os"
-	"image"
-	"bytes"
 	"gitlab.com/axet/desktop/go"
 	"github.com/atotto/clipboard"
-	"time"
+	"github.com/therecipe/qt/core"
+	"github.com/therecipe/qt/gui"
+	"github.com/therecipe/qt/qml"
 )
-
-func mkgrid(orientation gtk.Orientation) *gtk.Grid {
-	grid, err := gtk.GridNew()
-	if err != nil {
-		log.Fatal("Unable to create grid:", err)
-	}
-	grid.SetOrientation(orientation)
-	return grid
-}
 
 type App struct {
 	configuration Configuration
 	systray       *desktop.DesktopSysTray
 	jobLogger     JobLogger
 	activeProject string ""
-	win           *gtk.Window
+	//	win           *gtk.Window
 }
 
 func (app *App) toClipboard(report map[string]int, week int) {
-	var buffer bytes.Buffer
-
-	local, _ := time.LoadLocation("Local")
-	monday := firstDayOfISOWeek(time.Now().Year(), week, local)
-	friday := monday.Add(5 * 24 * time.Hour)
-	buffer.WriteString(fmt.Sprintf("%s - %s\n", monday.Format("2006-01-02"), friday.Format("2006-01-02")))
-	for k, v := range report {
-		buffer.WriteString(fmt.Sprintf("**%s** - %d%%\n", k, v))
-	}
-
-	if err := clipboard.WriteAll(buffer.String()); err != nil {
+	reportStr := snapshotToString(report, week)
+	if err := clipboard.WriteAll(reportStr); err != nil {
 		panic(err)
 	}
 }
@@ -51,151 +31,77 @@ func (app *App) CopyThisWeekToClipboard(mn *desktop.Menu) {
 	report, week := app.jobLogger.ThisWeekSnapshot()
 	app.toClipboard(report, week)
 }
+
 func (app *App) CopyPrevWeekToClipboard(mn *desktop.Menu) {
 	report, week := app.jobLogger.PrviousWeekSnapshot()
 	app.toClipboard(report, week)
 }
-func (app *App) initWindow() {
-	log.Info("Window creation...")
-	// Create a new toplevel window, set its title, and connect it to the
-	// "destroy" signal to exit the GTK main loop when it is destroyed.
-	win, err := gtk.WindowNew(gtk.WINDOW_POPUP)
-	if err != nil {
-		log.Fatal("Unable to create window:", err)
-	}
 
-	app.win = win
-	app.win.SetTitle("What are you doing now?")
-	app.win.Resize(300, 50)
-	app.win.SetBorderWidth(10)
-	app.win.SetPosition(gtk.WIN_POS_CENTER)
-	app.win.Connect("destroy", func() {
-		gtk.MainQuit()
+type QmlBride struct {
+	core.QObject
+	_         func(project string) `slot:"okPressed"`
+	_         func() `slot:"copyThisWeekPressed"`
+	_         func() `slot:"copyPrevWeekPressed"`
+	_         func() `slot:"windowClosed"`
+	_         func() uint64 `slot:"showPeriod"`
+	_         func() `constructor:"init"`
+
+	jobLogger JobLogger
+	clipboard *gui.QClipboard
+	showPeriodMs uint64
+}
+
+func (bridge *QmlBride) copy(s string) {
+	bridge.clipboard.SetText(s, gui.QClipboard__Clipboard)
+}
+
+func (bridge *QmlBride) init() {
+	bridge.ConnectOkPressed(func(project string) {
+		bridge.jobLogger.AddForNow(project)
+		fmt.Println(" go: OK!" + project)
+	})
+	bridge.ConnectWindowClosed(func() {
+		report, _ := bridge.jobLogger.ThisWeekSnapshot()
+		for key, value := range report {
+			fmt.Println("Key:", key, "Value:", value)
+		}
+	})
+	bridge.ConnectCopyThisWeekPressed(func() {
+		bridge.copy(snapshotToString(bridge.jobLogger.ThisWeekSnapshot()))
+	})
+	bridge.ConnectCopyPrevWeekPressed(func() {
+		bridge.copy(snapshotToString(bridge.jobLogger.PrviousWeekSnapshot()))
+	})
+	bridge.ConnectShowPeriod(func() uint64 {
+		return bridge.showPeriodMs
 	})
 
-	grid := mkgrid(gtk.ORIENTATION_VERTICAL)
-
-	l, err := gtk.LabelNew("What are you doing now?")
-	if err != nil {
-		log.Fatal("Unable to create label:", err)
-	}
-	l.SetMarginBottom(5)
-
-	grid2 := mkgrid(gtk.ORIENTATION_HORIZONTAL)
-
-	comboBox, err := gtk.ComboBoxTextNew()
-	if err != nil {
-		log.Fatal("Unable to create combo box:", err)
-	}
-	comboBox.AppendText("Other")
-	for _, project := range app.configuration.Projects {
-		comboBox.AppendText(project)
-	}
-
-	comboBox.Connect("changed", app.on_change)
-
-	comboBox.SetSizeRequest(270, 20)
-
-	button, err := gtk.ButtonNew()
-	if err != nil {
-		log.Fatal("Unable to create button:", err)
-	}
-	button.SetLabel("OK")
-	button.Connect("clicked", app.on_button)
-
-	grid2.Add(comboBox)
-	grid2.Add(button)
-
-	grid.Add(l)
-	grid.Add(grid2)
-
-	app.win.Add(grid)
-	log.Info("Window created")
-}
-
-func (app *App) show() {
-	log.Info("Show window")
-	if !app.win.GetNoShowAll() {
-		glib.IdleAdd(app.win.ShowAll)
-	} else {
-		app.jobLogger.AddForNow("===")
-	}
-}
-
-func (app *App) on_change(text *gtk.ComboBoxText) {
-	app.activeProject = text.GetActiveText()
-	app.changeButtonState()
-}
-
-func (app *App) on_button() {
-	app.jobLogger.AddForNow(app.activeProject)
-	log.Info("Hide window")
-	app.win.Hide()
-	report, _ := app.jobLogger.ThisWeekSnapshot()
-	for key, value := range report {
-		fmt.Println("Key:", key, "Value:", value)
-	}
-}
-
-func (app *App) changeButtonState() {
-
-}
-
-func loadIcon(app App) image.Image {
-	file, err := os.Open("icon.png")
-	if err != nil {
-		panic(err)
-	}
-	icon, _, err := image.Decode(file)
-	if err != nil {
-		panic(err)
-	}
-	return icon
+	var obj = core.NewQObject(nil)
+	obj.SetObjectName("objectName")
 }
 
 func main() {
-
 	log.SetFormatter(&log.TextFormatter{FullTimestamp:true, TimestampFormat:"2006-01-02 15:04:05 -0700"})
 	log.Info("Starting...")
 
 	configuration := load("")
 	log.Infof("Loaded configuration %+v", configuration)
 
-	app := App{
-		systray:desktop.DesktopSysTrayNew(),
-		jobLogger:FileJobLogger{
-			Basedir:configuration.LogPath,
-		},
-		configuration: configuration,
-	}
-	icon := loadIcon(app)
-	menu := []desktop.Menu{
-		desktop.Menu{
-			Type: desktop.MenuItem,
-			Enabled: true,
-			Name: "Copy this week report to clipboard",
-			Action: app.CopyThisWeekToClipboard,
-		},
-		desktop.Menu{
-			Type: desktop.MenuItem,
-			Enabled: true,
-			Name: "Copy prev week report to clipboard",
-			Action: app.CopyPrevWeekToClipboard,
-		},
-	}
+	jobLogger := FileJobLogger{Basedir:configuration.LogPath}
 
-	app.systray.SetIcon(icon)
-	app.systray.SetTitle("What are you doing now?")
-	app.systray.SetMenu(menu)
-	app.systray.Show()
+	gui.NewQGuiApplication(len(os.Args), os.Args)
 
-	app.initWindow()
+	bridge := NewQmlBride(nil)
+	bridge.jobLogger = jobLogger
+	bridge.clipboard = gui.QGuiApplication_Clipboard()
+	bridge.showPeriodMs = uint64(configuration.AskPeriodMin * 60000)
 
-	s := gocron.NewScheduler()
-	log.Info(fmt.Sprintf("Scheduling cron task for every %d minutes", app.configuration.AskPeriodMin))
-	s.Every(uint64(app.configuration.AskPeriodMin)).Minutes().Do(app.show)
-	s.Start()
+	engine := qml.NewQQmlApplicationEngine(nil)
+	engine.Load(core.NewQUrl3("qrc:/qml/application.qml", 0))
+	ctxt := engine.RootContext()
 
-	app.show()
-	desktop.Main()
+	ctxt.SetContextProperty("bridge", bridge)
+	ctxt.SetContextProperty("projectList", core.NewQStringListModel2(configuration.Projects, nil))
+
+	gui.QGuiApplication_Exec()
 }
